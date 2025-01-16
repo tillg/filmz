0c480b6df1bb515ebb0e8d6f50d2ab6a382e3fc6
Till Gartner <till.gartner@mgm-tp.com>
2025-01-16 09:46:17 +0100

    Initial Commit

---
+++ Filmz/FilmzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmzApp.swift
+//  Filmz
+//
+//  Created by Till Gartner on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct FilmzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FilmzTests/FilmzTests.swift
@@ -0,0 +1,17 @@
+//
+//  FilmzTests.swift
+//  FilmzTests
+//
+//  Created by Till Gartner on 16.01.25.
+//
+
+import Testing
+@testable import Filmz
+
+struct FilmzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
